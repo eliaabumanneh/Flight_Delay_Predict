f5c6eb869592f4c803b01c5702eff3195ff57ef7
Elia Abu-Manneh <e_abumanneh@yahoo.com>
2023-04-11 22:08:13 -0400

    updates

--- FlightDelayPredict/main.py
+++ FlightDelayPredict/main.py
@@ -2,34 +2,33 @@ import streamlit as st
 import os
 import pandas as pd
 import numpy as np
-#from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-#from sklearn.utils import shuffle
-#from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
-#from sklearn.model_selection import train_test_split
-#import tensorflow as tf
-#import keras
-#from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
-#from keras.models import Sequential
-#from keras.callbacks import Callback, ReduceLROnPlateau, EarlyStopping
-#from tensorflow.keras.losses import SparseCategoricalCrossentropy, CategoricalCrossentropy
-#from tensorflow.keras import datasets, layers, models
-#from IPython.display import clear_output
-#import matplotlib.pyplot as plt
-#import tensorflow.keras.backend as K
-#import keras.backend as K
-#from keras.models import load_model
-#from keras.utils import custom_object_scope
-#import statsmodels.api as sm
-#from sklearn.metrics import r2_score
-#import xgboost as xgb
-#from sklearn.metrics import mean_squared_error, mean_absolute_error
-#np.random.seed(0)
-#import seaborn as sns
-#from datetime import datetime
-#import PIL
-#from PIL import Image
-#import seaborn as sb
-#import math
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.utils import shuffle
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import keras
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.models import Sequential
+from keras.callbacks import Callback, ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.losses import SparseCategoricalCrossentropy, CategoricalCrossentropy
+from tensorflow.keras import datasets, layers, models
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+import tensorflow.keras.backend as K
+import keras.backend as K
+from keras.models import load_model
+from keras.utils import custom_object_scope
+import statsmodels.api as sm
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+np.random.seed(0)
+import seaborn as sns
+from datetime import datetime
+import PIL
+from PIL import Image
+import seaborn as sb
+import math
 
 
 #Streamlit Componenets
